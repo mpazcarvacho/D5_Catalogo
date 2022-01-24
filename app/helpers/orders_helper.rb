32914f32f45eaaeaacc19781afe821aad7d60397
mariapaz <mpazcarvacho@gmail.com>
2022-01-24 00:43:15 -0300

    basic form with payment options created

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,10 +1,20 @@
 module OrdersHelper
 
   def payment_platforms_for_select
-    PaymentPlatform.all.map{|p| p.name}
+    payments = []
+    PaymentPlatform.all.each do |pp|
+      if pp.payment_methods.all.count == 0
+        payments.push(pp.name)
+      else
+        pp.payment_methods.each do |pm|
+          payments.push("#{pp.name} - #{pm.name}")
+        end
+      end
+    end
+    payments
   end
 
   def payment_methods_for_select
-    PaymentMethod.all.map{|p| p.name}
+    PaymentMethod.all.map{|p| p.name + " - " + p.payment_platforms.name}
   end
 end
